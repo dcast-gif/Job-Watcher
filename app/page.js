bf9b06ac63e188855d427fa866861a3364a2ac55
dcast-gif <dylancastell@gmail.com>
2026-06-12 15:29:39 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -127,6 +127,7 @@ export default function HomePage() {
       });
 
       const data = await response.json();
+      alert("API returned");
       const debugInfo = data.debug || [];
 
       setDiagnostics(debugInfo);
